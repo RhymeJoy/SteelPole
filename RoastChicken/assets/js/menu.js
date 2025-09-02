ae7e026093281c165f98a56a341584da11042c76
欣韻 <t40932203@gmail.com>
2025-09-02 18:11:58 +0800

    Update menu.js

--- RoastChicken/assets/js/menu.js
+++ RoastChicken/assets/js/menu.js
@@ -46,7 +46,7 @@ const menuItems = [
     onclick: "return false;",
     price: "時價",
     description: "精選新鮮魚隻，搭配醬油與蔥段清蒸，完美保留魚肉鮮味與滑嫩口感。每一口都能感受到鮮美的自然風味，清爽又健康。(龍虎班目前一份500元)",
-    img: "assets/img/menu/a la carte/low/Steamed-fish.jpg",
+    img: "assets/img/menu/a la carte/low/steamed-fish.jpg",
     alt: "蒸魚",
     filter: "filter-dish"
   },
@@ -913,4 +913,5 @@ const observer = new IntersectionObserver(entries => {
 });
 
 lazyImages.forEach(img => observer.observe(img));
+
 
